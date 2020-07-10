e5a4a6a07fa6d60bb612358f15a774c647f22f6c
Ashley <ashley.bergsma42@gmail.com>
2020-07-09 21:01:47 -0700

    done with MVP, working on stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
+
 const card = document.querySelector('.cards');
 const myUserData = 'https://api.github.com/users/ashley-bergsma';
+
 
 /*
   STEP 1: using axios, send a GET request to the following URL
@@ -8,17 +11,19 @@ const myUserData = 'https://api.github.com/users/ashley-bergsma';
     https://api.github.com/users/<your name>
 */
 
-// axios.get('https://api.github.com/users/ashley-bergsma').then(response => {
-//     console.log(response);
-// }).catch(error => {
-//     console.log('there has been an error' + error);
-// })
+axios.get('https://api.github.com/users/ashley-bergsma').then(response => {
+    console.log(response);
+}).catch(error => {
+    console.log('there has been an error' + error);
+})
 
 axios.get(myUserData).then(response => {
-  card.appendChild(cardMaker(response))
+  card.prepend(cardMaker(response))
 }).catch(error => {
   console.log('gotta catch \'em all!' + error)
 });
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -43,7 +48,20 @@ axios.get(myUserData).then(response => {
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'];
+
+followersArray.forEach(item => {
+  axios.get('https://api.github.com/users/' + item)
+  .then(response => {
+    card.appendChild(cardMaker(response));
+  }).catch(error => {
+    console.log('gotta catch \'em all' + error);
+  })
+});
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -61,6 +79,14 @@ const followersArray = [];
         <p>Followers: {users followers count}</p>
         <p>Following: {users following count}</p>
         <p>Bio: {users bio}</p>
+        <button>More Info</button> 
+        <div class="more-info closed">
+            <p class="contact"> Contact: {users email}</p> 
+            <p>Repos: 
+            <a href = {address to repos page}>{address to users github repos page}</a> 
+            </p>
+
+        </div>
       </div>
     </div>
 */
@@ -90,32 +116,34 @@ userHandle.classList.add('username');
 card.appendChild(userImg); 
 // append card-info div to card div 
 card.appendChild(cardInfo); 
+// append the profile link to the paragraph 
+userProfile.textContent = 'Profile: '
+userProfile.appendChild(profileLink);
 // append all the items to the card info 
 cardInfo.appendChild(userName); 
 cardInfo.appendChild(userHandle);
 cardInfo.appendChild(userLocale);
 cardInfo.appendChild(userProfile);
-cardInfo.appendChild(profileLink);
 cardInfo.appendChild(userFollowers);
 cardInfo.appendChild(userFollows);
 cardInfo.appendChild(userBio);
 // testing, testing, is this thing on?
-console.log(card)
+// console.log(card)
 // adding content to the elements 
-userImg.src = 'user.data.avatar_url'; 
-userName.textContent = 'user.data.name'; 
-userHandle.textContent = 'user.data.login'; 
-userLocale.textContent = 'user.data.location'; 
-userProfile.textContent = 'Profile: ' // this is hardcoded to house the link
-profileLink.textContent = 'user.data.html_url'; 
-userFollowers.textContent = 'Followers: ${user.data.followers}'; 
-userFollows.textContent = 'Following: ${user.data.following}'; 
-userBio.textContent = 'Bio: ${user.data.bio}';
+userImg.src = user.data.avatar_url; 
+userName.textContent = user.data.name; 
+userHandle.textContent = user.data.login; 
+userLocale.textContent = user.data.location; 
+ // this is hardcoded to house the link
+profileLink.textContent = user.data.html_url; 
+userFollowers.textContent = 'Followers: ' + user.data.followers; 
+userFollows.textContent = 'Following: ' + user.data.following; 
+userBio.textContent = 'Bio: ' + user.data.bio;
 // THOU MUST RETURN 
 return card
 }
 
-cardMaker(myUserData);
+// cardMaker();
 /*
   List of LS Instructors Github username's:
     tetondan
